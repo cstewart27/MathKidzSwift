1d5b7cec85a9d085e9f52906f2d422c2abd3c55f
Christopher Stewart <christopherstewartsep@gmail.com>
2022-04-27 19:09:17 -0400

    Initial Commit

---
+++ MathKidz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MathKidz
+//
+//  Created by Christopher Stewart on 4/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MathKidz/MathKidzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathKidzApp.swift
+//  MathKidz
+//
+//  Created by Christopher Stewart on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct MathKidzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
